c20f6a4d2206272e5bd75515253154e3b0d8af17
Ocampo, Juan <Juan.Ocampo@McKesson.com>
2023-10-23 10:52:06 -0500

    Initial Commit

---
+++ src/main/java/org/example/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends HomePage {
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+}
