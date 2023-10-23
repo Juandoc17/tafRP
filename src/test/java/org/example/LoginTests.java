66b9fe070336e17428b676cd9ab065088075a684
Ocampo, Juan <Juan.Ocampo@McKesson.com>
2023-10-23 10:52:06 -0500

    Initial Commit

---
+++ src/test/java/org/example/LoginTests.java
@@ -0,0 +1,36 @@
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+public class LoginTests extends BaseTests {
+
+    private EventFiringWebDriver driver;
+    private static final Logger logger = LogManager.getLogger(LoginTests.class);
+
+    @Test
+    public void testLogin() {
+        // Find the username and password fields and the login button
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        logger.info("Typing the credentials in LoginPage");
+        usernameField.sendKeys("your_username");
+        passwordField.sendKeys("your_password");
+
+        logger.info("Login Action Button");
+        loginButton.click();
+        logger.info("Clicked the login button.");
+
+        WebElement dashboardHeader = driver.findElement(By.id("dashboardHeader"));
+        assert dashboardHeader.isDisplayed() : "Login failed.";
+        logger.info("Login successful.");
+    }
+
+}
---
+++ src/test/java/org/example/TestListener.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    private static ExtentReports extent = ExtentManager.createInstance("extent.html");
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
